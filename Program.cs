fe4269b8cc4ffcdbcaed74212f5abbd0f49b7a4e
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-01 20:15:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AttributedCarLibrary;
+
+namespace VehicleDescriptionAttributeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
+            ReflectOnAttributesUsingEarlyBinding();
+            Console.ReadLine();
+        }
+        static void ReflectOnAttributesUsingEarlyBinding()
+        {
+            Type t = typeof(Winnebago);
+
+            object[] customAtts = t.GetCustomAttributes(false);
+
+            foreach (VehicleDescriptionAttribute v in customAtts)
+                Console.WriteLine("->{0}", v.Description);
+        }
+    }
+}
