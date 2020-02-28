1e96f98038bd7762a851a486cfb4cc7afe098b3b
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-28 10:46:37 -0500

    Clean up code

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using AttributedCarLibrary;
+﻿using AttributedCarLibrary;
+using System;
 
 namespace VehicleDescriptionAttributeReader
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
             ReflectOnAttributesUsingEarlyBinding();
